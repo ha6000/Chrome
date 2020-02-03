320aacf4d0f4125c0b5ce6af0905363df7d67ef3
Non Hacker <krugerh27@gmail.com>
2020-02-03 19:58:57 +0200

    Hey
    I exploiter the premium thingy (aka giving me free premium (sorry))
    But I installed a whitelist thingy, so people cant just use it(unless they do what I just did)
    this was actually a experiment
    so, dont ban me

--- extension/js/rplus/premium.js
+++ extension/js/rplus/premium.js
@@ -5,56 +5,15 @@ RPlus.premium = RPlus.premium || (function () {
 
 	return {
 		getPremium: $.promise.cache(function (resolve, reject, userId) {
-			if (typeof (userId) !== "number") {
-				reject([{
-					code: 0,
-					message: "Invalid userId"
-				}]);
-				return;
-			}
-
-			if (userId <= 0) {
-				resolve(null);
-				return;
-			} else if (knownPremiums.hasOwnProperty(userId)) {
-				resolve(knownPremiums[userId]);
-				return;
-			}
-
-			var premiumBackup = function () {
-				return new Promise(function (resolve, reject) {
-					$.get("https://api.roblox.plus/v1/rpluspremium/" + userId).done(function (data) {
-						if (data.data) {
-							knownPremiums[userId] = {
-								expiration: data.data.expiration ? new Date(data.data.expiration).getTime() : null
-							};
-							resolve(knownPremiums[userId]);
-							RPlus.notifiers.catalog.updateToken();
-						} else {
-							resolve(null);
-						}
-					}).fail(function () {
-						reject([{ code: 0, message: "HTTP Request Failed" }]);
-					});
-				});
-			};
-
-			Roblox.games.getVipServers(105689058).then(function (vipServers) {
-				var currentDateTime = +new Date;
-				for (var n = 0; n < vipServers.length; n++) {
-					if (vipServers[n].owner.id === userId && vipServers[n].expirationDate >= currentDateTime) {
-						knownPremiums[userId] = {
-							expiration: vipServers[n].expirationDate
-						};
-						resolve(knownPremiums[userId]);
-						return;
-					}
-				}
-
-				premiumBackup().then(resolve).catch(reject);
-			}).catch(function () {
-				premiumBackup().then(resolve).catch(reject);
-			});
+			$.get("https://robloxpluslimit.glitch.me/?name=" + userId).done(function (data) {
+				if (data.status === 'ok') {
+					knownPremiums[userId] = {};
+					resolve(knownPremiums[userId]);
+					RPlus.notifiers.catalog.updateToken();
+				} else {
+					resolve(null);
+				};
+			})
 		}, {
 			resolveExpiry: 15 * 1000,
 			rejectExpiry: 10 * 1000,
@@ -63,7 +22,7 @@ RPlus.premium = RPlus.premium || (function () {
 
 		isPremium: $.promise.cache(function (resolve, reject, userId) {
 			this.getPremium(userId).then(function(premium) {
-				resolve(premium ? true : false);
+				resolve(true);
 			}).catch(reject);
 		}, {
 			resolveExpiry: 15 * 1000,
